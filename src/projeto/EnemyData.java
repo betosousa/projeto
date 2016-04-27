9d6de091778eb122744db6373c36f616a4697bda
betosousa <avss@cin.ufpe.br>
2016-04-27 11:47:26 -0300

    27/04 - 3
    atirando de boas

--- src/projeto/EnemyData.java
+++ src/projeto/EnemyData.java
@@ -7,20 +7,19 @@ import robocode.ScannedRobotEvent;
 
 public class EnemyData implements Serializable {
 	private static final long serialVersionUID = 1L;
-	public double velocity, heading, x, y;
+	public double velocity, heading, x, y, bearing;
 	
 	public EnemyData(ScannedRobotEvent e, AdvancedRobot r){
-		double bearing = r.getHeadingRadians() + e.getBearingRadians();
+		bearing = r.getHeadingRadians() + e.getBearingRadians();
 		x = r.getX() + e.getDistance() * Math.sin(bearing);
 		y = r.getY() + e.getDistance() * Math.cos(bearing);
-
 		velocity = e.getVelocity();
 		heading = e.getHeadingRadians();
 	}
 	
 	public double relativeBearing(AdvancedRobot r){
 		double dx = x - r.getX(), dy = y - r.getY();
-		return Math.atan(dx/dy);
+		return (Math.PI/2.0) - Math.atan2(dy, dx);
 	}
 	
 	public double relativeDistance(AdvancedRobot r){
--- src/projeto/GFTargeting.java
+++ src/projeto/GFTargeting.java
@@ -8,7 +8,7 @@ import robocode.util.Utils;
 
 public class GFTargeting {
 	AdvancedRobot bot;
-	double bulletPower = Rules.MAX_BULLET_POWER;
+	double bulletPower = Rules.MAX_BULLET_POWER, diag, closeRange;
 
 	ArrayList<BulletWave> bulletWaves = new ArrayList<BulletWave>();
 	static int[] stats = new int[31]; // 31 is the number of unique GuessFactors we're using
@@ -17,11 +17,14 @@ public class GFTargeting {
 	
 	public GFTargeting(AdvancedRobot r){
 		bot = r;
+		closeRange = bot.getBattleFieldWidth()/5;
+		diag = Math.sqrt(bot.getBattleFieldWidth()*bot.getBattleFieldWidth()*2) - closeRange; 
 	}
 
 	public void aim(EnemyData enemy){
 		
-		double enemyBearing = enemy.relativeBearing(bot);
+		double enemyBearing = enemy.relativeBearing(bot), enemyDistance = enemy.relativeDistance(bot);
+		
 		
 		// Let's process the waves now:
 		for (int i=0; i < bulletWaves.size(); i++){
@@ -40,6 +43,8 @@ public class GFTargeting {
 		}
 		int[] currentStats = stats; // This seems silly, but I'm using it to show something else later
 
+		shootPower(enemyDistance);
+		
 		BulletWave newWave = new BulletWave(bot.getX(), bot.getY(), enemyBearing, bulletPower, direction, bot.getTime(), currentStats);
 
 		int bestindex = 15;	// initialize it to be in the middle, guessfactor 0.
@@ -53,12 +58,25 @@ public class GFTargeting {
 		double gunAdjust = Utils.normalRelativeAngle(enemyBearing - bot.getGunHeadingRadians() + angleOffset);
 		bot.setTurnGunRightRadians(gunAdjust);
 
-		if (bot.getGunHeat() == 0 && gunAdjust < Math.atan2(9, enemy.relativeDistance(bot)) && bot.setFireBullet(bulletPower) != null) {
+		if (bot.getGunHeat() == 0 && gunAdjust < Math.atan2(9, enemyDistance) && bot.setFireBullet(bulletPower) != null) {
 			bulletWaves.add(newWave);
 		}
 
 	}
 	
+	public void shootPower(double distance) {
+		
+		if (distance < closeRange) {
+			bulletPower = Rules.MAX_BULLET_POWER;
+		} else {
+			double distanceRelativa = diag - distance;
+			if (distanceRelativa < 0)
+				bulletPower = Rules.MIN_BULLET_POWER;
+			else
+				bulletPower = Rules.MAX_BULLET_POWER*(distanceRelativa)/diag;			
+		}
+		bot.out.println(bulletPower);
+	}	
 	
 	
 }
